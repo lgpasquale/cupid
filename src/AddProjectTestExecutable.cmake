a979bc3b4729a2e91186ae4fb9368f1862ee4215
Luca Pasquale <luca.pasquale@moxoff.com>
2016-12-24 10:23:02 +0100

    Fixed AddProjectTestExecutable.cmake

--- src/AddProjectTestExecutable.cmake
+++ src/AddProjectTestExecutable.cmake
@@ -1,12 +1,12 @@
 # Create an executable and the relative test
 # Takes as arguments the target name and the list of source files
-# The test name can be specified with the NAME option
+# The test name can be specified with the TEST_NAME option
 # The arguments be passed to the executable can be specified with the ARGUMENTS option
 FUNCTION(ADD_PROJECT_TEST_EXECUTABLE TARGET_NAME)
     # Parse the arguments (accepted arguments are COMPONENTS and OPTIONAL_COMPONENTS)
     # and store their values in DEPENDENCY_COMPONENTS and DEPENDENCY_OPTIONAL_COMPONENTS
     set(OPTION_NAMES)
-    set(ONE_VALUE_ARG_NAMES NAME)
+    set(ONE_VALUE_ARG_NAMES TEST_NAME)
     set(MULTIPLE_VALUE_ARG_NAMES ARGUMENTS)
     cmake_parse_arguments(TEST "${OPTION_NAMES}" "${ONE_VALUE_ARG_NAMES}" "${MULTIPLE_VALUE_ARG_NAMES}" ${ARGN})
 
@@ -15,7 +15,7 @@ FUNCTION(ADD_PROJECT_TEST_EXECUTABLE TARGET_NAME)
     add_executable(${TARGET_NAME} ${SOURCES})
     target_link_libraries(${TARGET_NAME} ${LIBRARIES})
     # This allows the test to be executed with ctest
-    add_test(NAME ${TEST_NAME}
+    add_test(NAME ${TEST_TEST_NAME}
         COMMAND ${TARGET_NAME} ${TEST_ARGUMENTS}) # this is the command that will be executed to run the test
 
 ENDFUNCTION()
