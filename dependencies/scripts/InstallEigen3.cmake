30198778adbcc59c28c0dbc7f6b97ad88b217402
lgpasquale <lgpasquale@gmail.com>
2020-09-09 15:35:34 +0200

    Changed MD5 for eige

--- dependencies/scripts/InstallEigen3.cmake
+++ dependencies/scripts/InstallEigen3.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
     EIGEN3
     URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.0/eigen-3.3.0.tar.bz2"
-    URL_MD5 "fd1ecefaacc9223958b6a66f9a348424"
+    URL_MD5 "26aac7f87f3a50ca4db22a356b7f2c43"
     PREFIX ${DEPENDENCY_BASE_DIR}
     DOWNLOAD_DIR ${DEPENDENCY_ARCHIVE_DIR}
     CMAKE_CACHE_ARGS "-DCMAKE_INSTALL_PREFIX:PATH=${DEPENDENCY_INSTALL_DIR}"
