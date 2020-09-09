53d0dfe274e2dd49372aeff983428dfdc478744b
lgpasquale <lgpasquale@gmail.com>
2020-09-09 15:12:29 +0200

    Changed URL for Eigen repo

--- dependencies/scripts/InstallEigen3.cmake
+++ dependencies/scripts/InstallEigen3.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(
     EIGEN3
-    URL "http://bitbucket.org/eigen/eigen/get/3.3.0.tar.bz2"
+    URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.0/eigen-3.3.0.tar.bz2"
     URL_MD5 "fd1ecefaacc9223958b6a66f9a348424"
     PREFIX ${DEPENDENCY_BASE_DIR}
     DOWNLOAD_DIR ${DEPENDENCY_ARCHIVE_DIR}
